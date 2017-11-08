499f7300c9c736294c8a31595c6dfe30bf8ec2fc
Rob Looby <robertjlooby@gmail.com>
2017-11-07 22:40:12 -0600

    Make existing fields match the upload template

--- test/FitnessSpec.hs
+++ test/FitnessSpec.hs
@@ -11,14 +11,14 @@ import Types
 spec :: Spec
 spec = describe "Fitness" $ do
     it "is the sum of the projected scores of players on the team" $
-        let player = Player "name" QB 100 "" 0 (TeamName "chi")
+        let player = Player "name" QB 100 "" (TeamName "chi")
             p = PlayerWithProjected player 10
             team = Team p p p p p p p p p
         in
             fitness 1000 team `shouldBe` 90
 
     it "is 0 if the team is over the salary cap" $
-        let player = Player "name" QB 100 "" 0 (TeamName "chi")
+        let player = Player "name" QB 100 "" (TeamName "chi")
             p = PlayerWithProjected player 10
             team = Team p p p p p p p p p
         in
