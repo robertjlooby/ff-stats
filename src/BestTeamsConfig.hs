a3b6b07cfbc67dcc232182f345e6d4ad563a7b07
Rob Looby <robertjlooby@gmail.com>
2018-09-08 16:58:31 -0500

    Add StateT to App type

--- src/BestTeamsConfig.hs
+++ src/BestTeamsConfig.hs
@@ -7,12 +7,14 @@ module BestTeamsConfig
   ) where
 
 import Control.Monad.Trans.Reader (ReaderT, ask, asks, runReaderT)
+import Control.Monad.Trans.State.Lazy (StateT, evalStateT)
 import Dhall (Generic, Interpret)
+import System.Random (StdGen)
 
-type App = ReaderT Config IO
+type App = ReaderT Config (StateT StdGen IO)
 
-runApp :: App a -> Config -> IO a
-runApp = runReaderT
+runApp :: App a -> Config -> StdGen -> IO a
+runApp app config = evalStateT (runReaderT app config)
 
 data Config = Config
   { _crossoverProb :: Double
